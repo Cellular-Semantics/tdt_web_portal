22818230d4d2ffb0eb524b9fd3551fb0fe64ad3a
hkir-dev <huseyin_kir@yahoo.com>
2024-12-18 11:36:37 +0000

    login redirect exp

--- middleware.ts
+++ middleware.ts
@@ -1,11 +1,16 @@
 import { auth } from '@/lib/auth'
 import { NextResponse } from "next/server.js"
  
-const basePath = process.env.NEXT_PUBLIC_NEXT_CONFIG_BASE_PATH || '';
+const basePathEnv = process.env.NEXT_PUBLIC_NEXT_CONFIG_BASE_PATH || '';
 
 export default auth((req) => {
-  const protectedPaths = [`${basePath}/taxonomies`, "/taxonomies"];
+  const protectedPaths = [`${basePathEnv}/taxonomies`, "/taxonomies"];
 
+  const { pathname, search, origin, basePath } = req.nextUrl;
+  console.log("Request pathname: " + pathname);
+  console.log("Request search: " + search);
+  console.log("Request origin: " + origin);
+  console.log("Request basePath: " + basePath);
   // Check if the current path is protected
   const isProtectedPath = protectedPaths.some((path) =>
     req.nextUrl.pathname.startsWith(path)
@@ -13,14 +18,17 @@ export default auth((req) => {
 
   // This is needed because NextAuth relies on nextUrl.bathPath which doesn't seem to be defined
   if (isProtectedPath && !req.auth) {
-    console.log('Redirecting to login:', req.nextUrl.origin + basePath)
-    return NextResponse.redirect(
-      `${req.nextUrl.origin}${basePath}/login`
-    )
+    console.log('Redirecting to login:', req.nextUrl.origin + basePathEnv)
+    const redirectResponse = NextResponse.redirect(new URL(`${basePath}/login`, origin));
+    redirectResponse.headers.set('x-middleware-cache', 'no-cache'); // ! FIX: Disable caching
+    return redirectResponse;
+    // return NextResponse.redirect(
+    //   `${req.nextUrl.origin}${basePathEnv}/login`
+    // )
   }
 })
 
 // Don't invoke Middleware on some paths
 export const config = {
-  matcher: [`${basePath}/taxonomies/:path*`,`${basePath}/((?!api|_next/static|_next/image|favicon.ico|taxonomy.png|placeholder-user.jpg).*)`]
+  matcher: [`${basePathEnv}/taxonomies/:path*`,`${basePathEnv}/((?!api|_next/static|_next/image|favicon.ico|taxonomy.png|placeholder-user.jpg).*)`]
 };
