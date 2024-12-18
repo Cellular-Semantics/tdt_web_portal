703b75c2c2c0d3e44923666efbe5a322ab39f61d
hkir-dev <huseyin_kir@yahoo.com>
2024-12-18 12:06:34 +0000

    mwr exp

--- middleware.ts
+++ middleware.ts
@@ -18,7 +18,7 @@ export default auth((req) => {
 
   // This is needed because NextAuth relies on nextUrl.bathPath which doesn't seem to be defined
   if (isProtectedPath && !req.auth) {
-    const redirectUrl = new URL(`${basePathEnv}/login`, origin);
+    const redirectUrl = new URL(`${basePathEnv}/login`, "http://localhost:3000");
     console.log('Redirecting to:' + redirectUrl.toString());
     const redirectResponse = NextResponse.redirect(redirectUrl);
     redirectResponse.headers.set('x-middleware-cache', 'no-cache'); // ! FIX: Disable caching
